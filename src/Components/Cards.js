86e7df0c113263fed819b015135f9835b1fa9faa
aryan1403 <aaryan14032006@gmail.com>
2023-04-12 11:14:45 +0530

    created card component

---
+++ src/Components/Cards.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import './Cards.css';
+
+export default function Cards(props) {
+  return (
+    <div className="card">
+    <img className="card-img" src={props.img} alt={props.title} />
+    <div className="card-img-overlay">
+        <h3 className="card-title">{props.title}</h3>
+        <br/>
+        <h4 className="card-text">{props.content}</h4>
+        <br/>
+        <Link to={props.uri} className="btn btn-primary">{props.btn}</Link>
+    </div>
+    </div>
+  );
+}
