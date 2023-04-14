47daddc73a14837e675314044d03bc4a5fc8a213
aryan1403 <aaryan14032006@gmail.com>
2023-04-14 09:50:43 +0530

    small fix

--- src/Pages/whatsaapplogin.js
+++ src/Pages/whatsaapplogin.js
@@ -19,10 +19,10 @@ export default function WhatsAppLogin() {
         var c = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
         sendcode(c, number);
         alert('Code Sent Successfully to ' + number);
-
+        setverifycode(c);
         setisdis(false);
-        setverifycode(code);
         setbtntxt("Verify Code")
+        console.log(verifycode);
       } else {
         if(code === verifycode) {
           alert('Logged in Successfully');
@@ -52,18 +52,16 @@ export default function WhatsAppLogin() {
         <>
         <div className="additem">
         <h1 className="additemlabel">WhatsApp Login</h1>
-        <form className="form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="item-name">Enter your number:</label>
-          <input type="text" value={number} name="item-name" onChange={(e) => setnumber(e.target.value)} required />
+          <label>Enter your number:</label>
+          <input type="text" value={number} onChange={(e) => setnumber(e.target.value)} required />
         </div>
         <div hidden={isdis}>
-          <label htmlFor="item-name">Enter your code:</label>
-          <input type="text" value={code} name="item-name" onChange={(e) => setcode(e.target.value)} required />
+          <label>Enter your code:</label>
+          <input type="text" value={code} onChange={(e) => setcode(e.target.value)} required />
         </div>
-          <button style={{"width": "max-content"}} type="submit">{btntxt}</button>
-        </form>
-        </div>
+          <button onClick={handleSubmit} style={{"width": "max-content"}} type="submit">{btntxt}</button>
+          </div>
         </>
     );
 }
